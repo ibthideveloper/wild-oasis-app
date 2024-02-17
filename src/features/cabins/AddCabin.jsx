bbbff63e70036bf59d3274c6d60347daadb95a49
ibthideveloper <99708892+ibthideveloper@users.noreply.github.com>
2024-02-17 18:47:20 +0530

    ++ modal using compound component pattern

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,15 +1,24 @@
-import Button from 'ui/Button';
-import Modal from 'ui/Modal';
-import CreateCabinForm from './CreateCabinForm';
+// import Button from 'ui/Button';
+import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm-v1";
+import Button from "../../ui/Button";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
   return (
     <Modal>
-      <Modal.Toggle opens='new-cabin'>
-        <Button>Add new cabin</Button>
-      </Modal.Toggle>
-      <Modal.Window name='new-cabin'>
-        <CreateCabinForm />
+      <Modal.Open opens="cabin-form">
+        <Button>Add New Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm></CreateCabinForm>
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show Table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable></CabinTable>
       </Modal.Window>
     </Modal>
   );
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,17 +1,9 @@
-import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
-import Button from "../ui/Button";
-import CreateCabinForm from "../features/cabins/CreateCabinForm-v1";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  // useEffect(() => {
-  //   getCabins().then((data) => console.log(data));
-  // }, []);
-  const [showForm, setShowForm] = useState(false);
-
   return (
     <>
       <Row type="horizontal">
@@ -20,12 +12,8 @@ function Cabins() {
       </Row>
 
       <Row>
-        <CabinTable></CabinTable>
-
-        <Button onClick={() => setShowForm((show) => !show)}>
-          Add new Cabin
-        </Button>
-        {showForm && <CreateCabinForm></CreateCabinForm>}
+        <CabinTable />
+        <AddCabin />
       </Row>
     </>
   );
